0a9fb1fe35978aa9cf576c967aef327e9ec6bee2
Adam J. Arling <aarling@gmail.com>
2021-10-10 13:47:12 -0500

    Add podcast 2 promo video

--- components/sunday-jams.js
+++ components/sunday-jams.js
@@ -2,20 +2,28 @@ import React from "react";
 import { Box, Container, Flex, VStack } from "@chakra-ui/react";
 import BigButton from "./big-button";
 import Obfuscate from "react-obfuscate";
+import { AspectRatio } from "@chakra-ui/react";
 
 function SundayJams(props) {
   return (
     <Container pt="8" pb="12">
       <VStack spacing="8">
-        <Box>
+        <Box position="relative" paddingBottom="56.25%" h="0" width="100%">
           <iframe
-            title="Libsyn Player"
-            style={{ border: "none" }}
-            src="//html5-player.libsyn.com/embed/episode/id/20470658/height/90/theme/custom/thumbnail/yes/direction/forward/render-playlist/no/custom-color/000000/"
-            height="90"
-            width="100%"
-            scrolling="no"
-            allowFullScreen
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/k8b5ZhP4-3U"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
           ></iframe>
         </Box>
         <Box textAlign="center">
@@ -35,6 +43,17 @@ function SundayJams(props) {
           <BigButton href="https://forms.gle/yoN4NAZfZzkHSe7Y8" isExternal>
             RSVP to attend Sunday Jams
           </BigButton>
+        </Box>
+        <Box>
+          <iframe
+            title="Libsyn Player"
+            style={{ border: "none" }}
+            src="//html5-player.libsyn.com/embed/episode/id/20470658/height/90/theme/custom/thumbnail/yes/direction/forward/render-playlist/no/custom-color/000000/"
+            height="90"
+            width="100%"
+            scrolling="no"
+            allowFullScreen
+          ></iframe>
         </Box>
       </VStack>
     </Container>
